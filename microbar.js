ce17122d94e0a0d2efe21ece02467c13320e7c33
Manraj Singh <manrajsinghgrover@gmail.com>
2016-04-16 12:33:55 +0530

    Fixes a lot of things

--- microbar.js
+++ microbar.js
@@ -2,7 +2,7 @@
 * @Author: ManrajGrover
 * @Date:   2016-04-06 15:58:03
 * @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-04-15 12:46:13
+* @Last Modified time: 2016-04-16 12:32:50
 */
 
 (function(root, factory){
@@ -24,25 +24,22 @@
     function isColor(color){
         var el = document.createElement('div');
         el.style.backgroundColor = color;
-        if(el.style.backgroundColor.length == 0){
-            return false;
-        }
-        return true;
+        if(el.style.backgroundColor.length == 0)
+            throw new Error('Please check the color entered');
     }
 
     function transitionEndEventName () {
         var el = document.createElement('div'), transitions = { 'transition':'transitionend', 'OTransition':'otransitionend', 'MozTransition':'transitionend', 'WebkitTransition':'webkitTransitionEnd'};
         for(var i in transitions) {
-            if (transitions.hasOwnProperty(i) && el.style[i] !== undefined) {
+            if (transitions.hasOwnProperty(i) && el.style[i] !== undefined)
                 return transitions[i];
-            }
         }
+        return null;
     }
 
     function addStyleSheet(){
-        if(document.getElementById('microbarstyles') != undefined){
+        if(document.getElementById('microbarstyles') != undefined)
             return;
-        }
         var style = document.createElement('style');
         style.type = 'text/css', style.id = 'microbarstyles';
         if(style.styleSheet){
@@ -55,45 +52,43 @@
     }
 
     function initialize(id, color, percentage, speed){
-        addStyleSheet();
+        addStyleSheet();isColor(color);
         var bar = document.createElement('div'), progress = document.createElement('div'), shadow = document.createElement('div'), time = getEquivalentTime(speed);
         bar.id = id, bar.className = 'microbar';
         progress.classList.add('mprogress') , progress.style.backgroundColor = color, progress.style.width = percentage+'%', progress.style.transition = 'width '+time+' ease, opacity 0.3s ease';
         shadow.classList.add('mshadow'), shadow.style.boxShadow = "0 0 10px "+color;
-        if(percentage == 0){
+        if(percentage == 0)
             progress.style.opacity = 0;
-        }
         progress.appendChild(shadow);
         bar.appendChild(progress);
         var transitionEnd = transitionEndEventName();
+        if(transitionEnd == null)
+            throw new Error('Your browser is not supported.');
         progress.addEventListener(transitionEnd, function(){
             var width = this.style.width;
-            if(width == '100%'){
+            if(width == '100%')
                 this.style.opacity = 0;
-            }
         });
-        return bar;
+        return {bar: bar, progress: progress, shadow: shadow};
     }
 
     return function microbar(args){
         args = args || {};
         
-        if(!args.hasOwnProperty('id') || typeof args['id'] !== 'string'){
+        if(!args.hasOwnProperty('id') || typeof args['id'] !== 'string')
             throw new Error('You need to provide an ID of type string for your microbar.');
-        }
-        if(args.hasOwnProperty('speed') && (typeof args.speed !== 'number' || args.speed > 10 || args.speed < 1)){
+        if(args.hasOwnProperty('speed') && (typeof args.speed !== 'number' || args.speed > 10 || args.speed < 1))
             throw new Error('Speed should be an integer between 1 and 10.');
-        }
-        if(args.hasOwnProperty('percentage') && (typeof args.percentage !== 'number' || args.percentage> 100 || args.percentage < 0)){
+        if(args.hasOwnProperty('percentage') && (typeof args.percentage !== 'number' || args.percentage> 100 || args.percentage < 0))
             throw new Error('Percentage should be an integer between 0 and 100.');
-        }
 
         var percentage = args.percentage || 0,
             color = args.color || '#000000',
             speed = args.speed || 10,
             id = args.id;
 
-        var bar = initialize(id, color, percentage, speed);
+        var divs = initialize(id, color, percentage, speed);
+        var bar = divs['bar'], progress = divs['progress'], shadow = divs['shadow'];
         if(args.target) {
             bar.style.position = 'relative';
             document.getElementById(args.target).insertBefore(bar, document.getElementById(args.target).firstChild);
@@ -105,31 +100,27 @@
         return {
             id: args.id,
             moveTo: function(percentage){
-                if(typeof percentage !== 'number' || percentage > 100 || percentage < 0){
+                if(typeof percentage !== 'number' || percentage > 100 || percentage < 0)
                     throw new Error('Percentage should be an integer between 0 and 100.');
-                }
-                bar.getElementsByClassName('mprogress')[0].style.opacity = 1;
-                bar.getElementsByClassName('mprogress')[0].style.width = percentage+'%';
+                progress.style.opacity = 1;
+                progress.style.width = percentage+'%';
             },
             getColor: function(){
                 return color;
             },
             setColor: function(color){
-                if(!isColor(color)){
-                    throw new Error('Please check color entered');
-                }
-                bar.getElementsByClassName('mprogress')[0].style.backgroundColor = color;
-                bar.getElementsByClassName('mshadow')[0].style.boxShadow = "0 0 10px "+color;
+                isColor(color);
+                progress.style.backgroundColor = color;
+                shadow.style.boxShadow = "0 0 10px "+color;
             },
             getSpeed: function(){
                 return speed;
             },
             setSpeed: function(s){
-                if(s > 10 || s < 1){
+                if(s > 10 || s < 1)
                     throw new RangeError('Speed should be an integer between 1 and 10');
-                }
                 speed = s;
-                bar.getElementsByClassName('mprogress')[0].style.transition = 'width '+getEquivalentTime(s);
+                progress.style.transition = 'width '+getEquivalentTime(s);
             }
         };
     };
